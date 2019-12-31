93055f8b71ee26b84078763efde15d7945afe20d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:36:14 -0600

    Rewriting as Laminas Project package

--- src/Adapter/Exception/RuntimeException.php
+++ src/Adapter/Exception/RuntimeException.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-paginator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-paginator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-paginator/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Paginator\Adapter\Exception;
+namespace Laminas\Paginator\Adapter\Exception;
 
-use Zend\Paginator\Exception;
+use Laminas\Paginator\Exception;
 
 class RuntimeException extends Exception\RuntimeException implements
     ExceptionInterface
