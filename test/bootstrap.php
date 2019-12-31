b6d31207aad167b0ff79afcd41f690e0ce15a49b
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:35:44 -0600

    Rewriting as Laminas Project package

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
- * @link      http://github.com/zendframework/zend-paginator for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-paginator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-paginator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-paginator/blob/master/LICENSE.md New BSD License
  */
 
 /*
