59f75f6290305598d6b9fdfe9548f8829da7d170
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:36:11 -0600

    Rewriting as Laminas Project package

--- src/ScrollingStyle/Jumping.php
+++ src/ScrollingStyle/Jumping.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-paginator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-paginator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-paginator/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Paginator\ScrollingStyle;
+namespace Laminas\Paginator\ScrollingStyle;
 
-use Zend\Paginator\Paginator;
+use Laminas\Paginator\Paginator;
 
 /**
  * A scrolling style in which the cursor advances to the upper bound
