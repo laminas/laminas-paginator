771bc2c203d548f763e3be52fb95be3bb8fe6917
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:35:37 -0600

    Rewriting as Laminas Project package

--- src/SerializableLimitIterator.php
+++ src/SerializableLimitIterator.php
@@ -1,20 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Paginator
+ * @see       https://github.com/laminas/laminas-paginator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-paginator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-paginator/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Paginator;
+namespace Laminas\Paginator;
 
 use Iterator;
 
 /**
- * @category   Zend
- * @package    Zend_Paginator
+ * @category   Laminas
+ * @package    Laminas_Paginator
  */
 class SerializableLimitIterator extends \LimitIterator implements \Serializable, \ArrayAccess
 {
@@ -34,7 +32,7 @@ class SerializableLimitIterator extends \LimitIterator implements \Serializable,
     private $count;
 
     /**
-     * Construct a Zend\Paginator\SerializableLimitIterator
+     * Construct a Laminas\Paginator\SerializableLimitIterator
      *
      * @param Iterator $it Iterator to limit (must be serializable by un-/serialize)
      * @param int $offset Offset to first element
--- test/ScrollingStyle/AllTest.php
+++ test/ScrollingStyle/AllTest.php
@@ -1,33 +1,31 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Paginator
+ * @see       https://github.com/laminas/laminas-paginator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-paginator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-paginator/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Paginator\ScrollingStyle;
+namespace LaminasTest\Paginator\ScrollingStyle;
 
-use Zend\Paginator\Paginator;
-use Zend\Paginator\Adapter\ArrayAdapter;
+use Laminas\Paginator\Adapter\ArrayAdapter;
+use Laminas\Paginator\Paginator;
 
 /**
- * @category   Zend
- * @package    Zend_Paginator
+ * @category   Laminas
+ * @package    Laminas_Paginator
  * @subpackage UnitTests
- * @group      Zend_Paginator
+ * @group      Laminas_Paginator
  */
 class AllTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var \Zend\Paginator\ScrollingStyle\All
+     * @var \Laminas\Paginator\ScrollingStyle\All
      */
     private $scrollingStyle = null;
 
     /**
-     * @var \Zend\Paginator\Paginator
+     * @var \Laminas\Paginator\Paginator
      */
     private $paginator = null;
 
@@ -37,7 +35,7 @@ class AllTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->scrollingStyle = new \Zend\Paginator\ScrollingStyle\All();
+        $this->scrollingStyle = new \Laminas\Paginator\ScrollingStyle\All();
         $this->paginator = new Paginator(new ArrayAdapter(range(1, 101)));
         $this->paginator->setItemCountPerPage(10);
     }
