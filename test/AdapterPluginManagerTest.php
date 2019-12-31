e9695e137210894d58e27905f7d06a5725629012
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:35:59 -0600

    Rewriting as Laminas Project package

--- test/AdapterPluginManagerTest.php
+++ test/AdapterPluginManagerTest.php
@@ -1,22 +1,21 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-paginator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-paginator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-paginator/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Paginator;
+namespace LaminasTest\Paginator;
 
-use Zend\Stdlib\CallbackHandler;
-use Zend\Db\Adapter\Platform\Sql92;
-use Zend\Paginator\AdapterPluginManager;
-use Zend\ServiceManager\ServiceManager;
-use Zend\Mvc\Service\ServiceManagerConfig;
+use Laminas\Db\Adapter\Platform\Sql92;
+use Laminas\Mvc\Service\ServiceManagerConfig;
+use Laminas\Paginator\AdapterPluginManager;
+use Laminas\ServiceManager\ServiceManager;
+use Laminas\Stdlib\CallbackHandler;
 
 /**
- * @group      Zend_Paginator
+ * @group      Laminas_Paginator
  */
 class AdapterPluginManagerTest extends \PHPUnit_Framework_TestCase
 {
@@ -30,19 +29,19 @@ class AdapterPluginManagerTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->adapaterPluginManager = new AdapterPluginManager();
-        $this->mockSelect = $this->getMock('Zend\Db\Sql\Select');
+        $this->mockSelect = $this->getMock('Laminas\Db\Sql\Select');
 
-        $mockStatement = $this->getMock('Zend\Db\Adapter\Driver\StatementInterface');
-        $mockResult = $this->getMock('Zend\Db\Adapter\Driver\ResultInterface');
+        $mockStatement = $this->getMock('Laminas\Db\Adapter\Driver\StatementInterface');
+        $mockResult = $this->getMock('Laminas\Db\Adapter\Driver\ResultInterface');
 
-        $mockDriver = $this->getMock('Zend\Db\Adapter\Driver\DriverInterface');
+        $mockDriver = $this->getMock('Laminas\Db\Adapter\Driver\DriverInterface');
         $mockDriver->expects($this->any())->method('createStatement')->will($this->returnValue($mockStatement));
         $mockStatement->expects($this->any())->method('execute')->will($this->returnValue($mockResult));
-        $mockPlatform = $this->getMock('Zend\Db\Adapter\Platform\PlatformInterface');
+        $mockPlatform = $this->getMock('Laminas\Db\Adapter\Platform\PlatformInterface');
         $mockPlatform->expects($this->any())->method('getName')->will($this->returnValue('platform'));
 
         $this->mockAdapter = $this->getMockForAbstractClass(
-            'Zend\Db\Adapter\Adapter',
+            'Laminas\Db\Adapter\Adapter',
             array($mockDriver, $mockPlatform)
         );
     }
@@ -50,17 +49,17 @@ class AdapterPluginManagerTest extends \PHPUnit_Framework_TestCase
     public function testCanRetrieveAdapterPlugin()
     {
         $plugin = $this->adapaterPluginManager->get('array', array(1, 2, 3));
-        $this->assertInstanceOf('Zend\Paginator\Adapter\ArrayAdapter', $plugin);
+        $this->assertInstanceOf('Laminas\Paginator\Adapter\ArrayAdapter', $plugin);
         $plugin = $this->adapaterPluginManager->get('iterator', new \ArrayIterator(range(1, 101)));
-        $this->assertInstanceOf('Zend\Paginator\Adapter\Iterator', $plugin);
+        $this->assertInstanceOf('Laminas\Paginator\Adapter\Iterator', $plugin);
         $plugin = $this->adapaterPluginManager->get('dbselect', array($this->mockSelect, $this->mockAdapter));
-        $this->assertInstanceOf('Zend\Paginator\Adapter\DbSelect', $plugin);
+        $this->assertInstanceOf('Laminas\Paginator\Adapter\DbSelect', $plugin);
         $plugin = $this->adapaterPluginManager->get('null', 101);
-        $this->assertInstanceOf('Zend\Paginator\Adapter\NullFill', $plugin);
+        $this->assertInstanceOf('Laminas\Paginator\Adapter\NullFill', $plugin);
 
         //test dbtablegateway
-        $mockStatement = $this->getMock('Zend\Db\Adapter\Driver\StatementInterface');
-        $mockDriver = $this->getMock('Zend\Db\Adapter\Driver\DriverInterface');
+        $mockStatement = $this->getMock('Laminas\Db\Adapter\Driver\StatementInterface');
+        $mockDriver = $this->getMock('Laminas\Db\Adapter\Driver\DriverInterface');
         $mockDriver->expects($this->any())
                    ->method('createStatement')
                    ->will($this->returnValue($mockStatement));
@@ -68,11 +67,11 @@ class AdapterPluginManagerTest extends \PHPUnit_Framework_TestCase
             ->method('formatParameterName')
             ->will($this->returnArgument(0));
         $mockAdapter = $this->getMockForAbstractClass(
-            'Zend\Db\Adapter\Adapter',
+            'Laminas\Db\Adapter\Adapter',
             array($mockDriver, new Sql92())
         );
         $mockTableGateway = $this->getMockForAbstractClass(
-            'Zend\Db\TableGateway\TableGateway',
+            'Laminas\Db\TableGateway\TableGateway',
             array('foobar', $mockAdapter)
         );
         $where  = "foo = bar";
@@ -83,7 +82,7 @@ class AdapterPluginManagerTest extends \PHPUnit_Framework_TestCase
             'dbtablegateway',
             array($mockTableGateway, $where, $order, $group, $having)
         );
-        $this->assertInstanceOf('Zend\Paginator\Adapter\DbTableGateway', $plugin);
+        $this->assertInstanceOf('Laminas\Paginator\Adapter\DbTableGateway', $plugin);
 
         //test callback
         $itemsCallback = new CallbackHandler(function () {
@@ -93,7 +92,7 @@ class AdapterPluginManagerTest extends \PHPUnit_Framework_TestCase
             return 0;
         });
         $plugin = $this->adapaterPluginManager->get('callback', array($itemsCallback, $countCallback));
-        $this->assertInstanceOf('Zend\Paginator\Adapter\Callback', $plugin);
+        $this->assertInstanceOf('Laminas\Paginator\Adapter\Callback', $plugin);
     }
 
     public function testCanRetrievePluginManagerWithServiceManager()
@@ -101,12 +100,12 @@ class AdapterPluginManagerTest extends \PHPUnit_Framework_TestCase
         $sm = $this->serviceManager = new ServiceManager(
             new ServiceManagerConfig(array(
                 'factories' => array(
-                    'PaginatorPluginManager'  => 'Zend\Mvc\Service\PaginatorPluginManagerFactory',
+                    'PaginatorPluginManager'  => 'Laminas\Mvc\Service\PaginatorPluginManagerFactory',
                 ),
             ))
         );
         $sm->setService('Config', array());
         $adapterPluginManager = $sm->get('PaginatorPluginManager');
-        $this->assertInstanceOf('Zend\Paginator\AdapterPluginManager', $adapterPluginManager);
+        $this->assertInstanceOf('Laminas\Paginator\AdapterPluginManager', $adapterPluginManager);
     }
 }
