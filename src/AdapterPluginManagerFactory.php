946557f8008c9f26984fb0de4102933fd68954cd
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-02-24 14:34:14 -0600

    feat: add support for "paginators" configuration
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- src/AdapterPluginManagerFactory.php
+++ src/AdapterPluginManagerFactory.php
@@ -9,6 +9,7 @@
 namespace Laminas\Paginator;
 
 use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Config;
 use Laminas\ServiceManager\FactoryInterface;
 use Laminas\ServiceManager\ServiceLocatorInterface;
 
@@ -28,7 +29,24 @@ class AdapterPluginManagerFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $name, array $options = null)
     {
-        return new AdapterPluginManager($container, $options ?: []);
+        $pluginManager = new AdapterPluginManager($container, $options ?: []);
+
+        // If we do not have a config service, nothing more to do
+        if (! $container->has('config')) {
+            return $pluginManager;
+        }
+
+        $config = $container->get('config')['paginators'] ?? null;
+
+        // If we do not have serializers configuration, nothing more to do
+        if (! is_array($config)) {
+            return $pluginManager;
+        }
+
+        // Wire service configuration for serializers
+        (new Config($config))->configureServiceManager($pluginManager);
+
+        return $pluginManager;
     }
 
     /**
