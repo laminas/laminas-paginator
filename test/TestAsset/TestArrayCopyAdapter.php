502c91472f6bef911e1f55eb5b4706f5d0b7cdfa
Emmanuel Belair <contact@ebelair.fr>
2022-11-18 15:38:55 +0100

    Inspection corrections
    Signed-off-by: Emmanuel Belair <contact@ebelair.fr>

--- test/TestAsset/TestArrayCopyAdapter.php
+++ test/TestAsset/TestArrayCopyAdapter.php
@@ -1,9 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaminasTest\Paginator\TestAsset;
 
 class TestArrayCopyAdapter extends TestAdapter
 {
+    /**
+     * @return array
+     */
     public function getArrayCopy()
     {
         return [];
