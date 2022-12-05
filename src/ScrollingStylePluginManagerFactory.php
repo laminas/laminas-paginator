231a05ed5d6e2c2648aa0b5f698b726809226802
George Steel <george@net-glue.co.uk>
2022-12-05 15:48:52 +0000

    Improve config-related type inference in factories
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/ScrollingStylePluginManagerFactory.php
+++ src/ScrollingStylePluginManagerFactory.php
@@ -6,30 +6,43 @@ namespace Laminas\Paginator;
 
 use Laminas\ServiceManager\FactoryInterface;
 use Laminas\ServiceManager\ServiceLocatorInterface;
+use Laminas\ServiceManager\ServiceManager;
 use Psr\Container\ContainerInterface;
 
+use function is_array;
+
+/**
+ * @psalm-import-type ServiceManagerConfiguration from ServiceManager
+ * @final
+ */
 class ScrollingStylePluginManagerFactory implements FactoryInterface
 {
     /**
      * laminas-servicemanager v2 support for invocation options.
      *
-     * @var array
+     * @var ServiceManagerConfiguration|null
      */
     protected $creationOptions;
 
     /**
      * {@inheritDoc}
      *
+     * @param string|null $requestedName
+     * @param ServiceManagerConfiguration|null $options
      * @return ScrollingStylePluginManager
      */
-    public function __invoke(ContainerInterface $container, $name, ?array $options = null)
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
-        return new ScrollingStylePluginManager($container, $options ?: []);
+        /** @psalm-var ServiceManagerConfiguration $options */
+        $options = ! is_array($options) ? [] : $options;
+        return new ScrollingStylePluginManager($container, $options);
     }
 
     /**
      * {@inheritDoc}
      *
+     * @param string|null $name
+     * @param string|null $requestedName
      * @return ScrollingStylePluginManager
      */
     public function createService(ServiceLocatorInterface $container, $name = null, $requestedName = null)
@@ -40,6 +53,7 @@ class ScrollingStylePluginManagerFactory implements FactoryInterface
     /**
      * laminas-servicemanager v2 support for invocation options.
      *
+     * @param ServiceManagerConfiguration $options
      * @return void
      */
     public function setCreationOptions(array $options)
