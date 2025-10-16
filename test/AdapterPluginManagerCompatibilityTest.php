503de155175b01d61e9de9045a575359b09a391b
George Steel <george@net-glue.co.uk>
2025-10-16 12:47:58 +0100

    Inline `CommonPluginManagerTrait` to gain PHPUnit 11.x compat and to decouple tests from service manager lib
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/AdapterPluginManagerCompatibilityTest.php
+++ test/AdapterPluginManagerCompatibilityTest.php
@@ -7,52 +7,35 @@ namespace LaminasTest\Paginator;
 use Iterator;
 use Laminas\Paginator\Adapter\AdapterInterface;
 use Laminas\Paginator\AdapterPluginManager;
-use Laminas\Paginator\Exception\RuntimeException;
+use Laminas\ServiceManager\Exception\InvalidServiceException;
 use Laminas\ServiceManager\ServiceManager;
-use Laminas\ServiceManager\Test\CommonPluginManagerTrait;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 use ReflectionProperty;
+use stdClass;
 
 use function assert;
+use function class_exists;
 use function is_string;
 use function str_contains;
 
 final class AdapterPluginManagerCompatibilityTest extends TestCase
 {
-    use CommonPluginManagerTrait;
-
-    /**
-     * @return AdapterPluginManager
-     */
-    protected static function getPluginManager()
+    protected static function getPluginManager(): AdapterPluginManager
     {
         return new AdapterPluginManager(new ServiceManager());
     }
 
     /**
-     * @return string
-     */
-    protected function getV2InvalidPluginException()
-    {
-        return RuntimeException::class;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getInstanceOf()
-    {
-        return AdapterInterface::class;
-    }
-
-    /**
-     * @return iterable<string, array{0: string, 1: string}>
+     * @return iterable<string, array{0: string, 1: class-string}>
      */
     public static function aliasProvider(): iterable
     {
         $pluginManager = self::getPluginManager();
         $r             = new ReflectionProperty($pluginManager, 'aliases');
         $aliases       = $r->getValue($pluginManager);
+        self::assertIsArray($aliases);
 
         foreach ($aliases as $alias => $target) {
             assert(is_string($alias) && is_string($target));
@@ -72,7 +55,66 @@ final class AdapterPluginManagerCompatibilityTest extends TestCase
                 continue;
             }
 
+            assert(class_exists($target));
+
             yield $alias => [$alias, $target];
         }
+    }
+
+    public function testInstanceOfMatches(): void
+    {
+        $manager    = $this->getPluginManager();
+        $reflection = new ReflectionProperty($manager, 'instanceOf');
+        $this->assertEquals(AdapterInterface::class, $reflection->getValue($manager), 'instanceOf does not match');
+    }
+
+    public function testShareByDefaultAndSharedByDefault(): void
+    {
+        $manager        = $this->getPluginManager();
+        $reflection     = new ReflectionClass($manager);
+        $shareByDefault = $sharedByDefault = true;
+
+        foreach ($reflection->getProperties() as $prop) {
+            if ($prop->getName() === 'shareByDefault') {
+                /** @psalm-var mixed $shareByDefault */
+                $shareByDefault = $prop->getValue($manager);
+                self::assertIsBool($shareByDefault);
+            }
+            if ($prop->getName() === 'sharedByDefault') {
+                /** @psalm-var mixed $sharedByDefault */
+                $sharedByDefault = $prop->getValue($manager);
+                self::assertIsBool($sharedByDefault);
+            }
+        }
+
+        $this->assertSame(
+            $shareByDefault,
+            $sharedByDefault,
+            'Values of shareByDefault and sharedByDefault do not match'
+        );
+    }
+
+    public function testRegisteringInvalidElementRaisesException(): void
+    {
+        $this->expectException(InvalidServiceException::class);
+        /** @psalm-suppress InvalidArgument */
+        $this->getPluginManager()->setService('test', $this);
+    }
+
+    public function testLoadingInvalidElementRaisesException(): void
+    {
+        $manager = $this->getPluginManager();
+        $manager->setInvokableClass('test', stdClass::class);
+        $this->expectException(InvalidServiceException::class);
+        $manager->get('test');
+    }
+
+    /**
+     * @param class-string $expected
+     */
+    #[DataProvider('aliasProvider')]
+    public function testPluginAliasesResolve(string $alias, string $expected): void
+    {
+        $this->assertInstanceOf($expected, $this->getPluginManager()->get($alias), "Alias '$alias' does not resolve'");
     }
 }
--- test/ScrollingStylePluginManagerCompatibilityTest.php
+++ test/ScrollingStylePluginManagerCompatibilityTest.php
@@ -4,38 +4,101 @@ declare(strict_types=1);
 
 namespace LaminasTest\Paginator;
 
-use Laminas\Paginator\Exception\InvalidArgumentException;
 use Laminas\Paginator\ScrollingStyle\ScrollingStyleInterface;
 use Laminas\Paginator\ScrollingStylePluginManager;
+use Laminas\ServiceManager\Exception\InvalidServiceException;
 use Laminas\ServiceManager\ServiceManager;
-use Laminas\ServiceManager\Test\CommonPluginManagerTrait;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use ReflectionProperty;
+use stdClass;
+
+use function assert;
+use function class_exists;
 
 final class ScrollingStylePluginManagerCompatibilityTest extends TestCase
 {
-    use CommonPluginManagerTrait;
+    public function testInstanceOfMatches(): void
+    {
+        $manager    = $this->getPluginManager();
+        $reflection = new ReflectionProperty($manager, 'instanceOf');
+        $this->assertEquals(
+            ScrollingStyleInterface::class,
+            $reflection->getValue($manager),
+            'instanceOf does not match',
+        );
+    }
+
+    public function testShareByDefaultAndSharedByDefault(): void
+    {
+        $manager        = $this->getPluginManager();
+        $reflection     = new ReflectionClass($manager);
+        $shareByDefault = $sharedByDefault = true;
+
+        foreach ($reflection->getProperties() as $prop) {
+            if ($prop->getName() === 'shareByDefault') {
+                /** @psalm-var mixed $shareByDefault */
+                $shareByDefault = $prop->getValue($manager);
+            }
+            if ($prop->getName() === 'sharedByDefault') {
+                /** @psalm-var mixed $sharedByDefault */
+                $sharedByDefault = $prop->getValue($manager);
+            }
+        }
+
+        $this->assertSame(
+            $shareByDefault,
+            $sharedByDefault,
+            'Values of shareByDefault and sharedByDefault do not match'
+        );
+    }
+
+    public function testRegisteringInvalidElementRaisesException(): void
+    {
+        $this->expectException(InvalidServiceException::class);
+        /** @psalm-suppress InvalidArgument */
+        $this->getPluginManager()->setService('test', $this);
+    }
+
+    public function testLoadingInvalidElementRaisesException(): void
+    {
+        $manager = $this->getPluginManager();
+        $manager->setInvokableClass('test', stdClass::class);
+        $this->expectException(InvalidServiceException::class);
+        $manager->get('test');
+    }
 
     /**
-     * @return ScrollingStylePluginManager
+     * @param class-string $expected
      */
-    protected static function getPluginManager()
+    #[DataProvider('aliasProvider')]
+    public function testPluginAliasesResolve(string $alias, string $expected): void
+    {
+        $this->assertInstanceOf($expected, $this->getPluginManager()->get($alias), "Alias '$alias' does not resolve'");
+    }
+
+    /**
+     * @return list<array{0: string, 1: class-string}>
+     */
+    public static function aliasProvider(): array
+    {
+        $manager    = self::getPluginManager();
+        $reflection = new ReflectionProperty($manager, 'aliases');
+        $data       = [];
+        foreach ($reflection->getValue($manager) as $alias => $expected) {
+            self::assertIsString($alias);
+            self::assertIsString($expected);
+            assert(class_exists($expected));
+
+            $data[] = [$alias, $expected];
+        }
+
+        return $data;
+    }
+
+    protected static function getPluginManager(): ScrollingStylePluginManager
     {
         return new ScrollingStylePluginManager(new ServiceManager());
-    }
-
-    /**
-     * @return string
-     */
-    protected function getV2InvalidPluginException()
-    {
-        return InvalidArgumentException::class;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getInstanceOf()
-    {
-        return ScrollingStyleInterface::class;
     }
 }
