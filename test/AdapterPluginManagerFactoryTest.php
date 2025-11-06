51989a0cec7f1e4a11c3dd8f0783ce1b471daab3
George Steel <george@net-glue.co.uk>
2025-11-06 08:32:21 +0000

    The `AdapterPluginManagerFactory` should accept `ArrayAccess` for configuration
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/AdapterPluginManagerFactoryTest.php
+++ test/AdapterPluginManagerFactoryTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace LaminasTest\Paginator;
 
+use ArrayObject;
 use Laminas\Paginator\Adapter\AdapterInterface;
 use Laminas\Paginator\AdapterPluginManagerFactory;
+use Laminas\ServiceManager\ServiceManager;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
@@ -88,5 +90,28 @@ final class AdapterPluginManagerFactoryTest extends TestCase
         $this->assertSame($adapter, $pluginManager->get('test'));
         $this->assertTrue($pluginManager->has('test-too'));
         $this->assertSame($adapter, $pluginManager->get('test-too'));
+    }
+
+    public function testConfigurationCanBeAnObject(): void
+    {
+        $adapter = $this->createMock(AdapterInterface::class);
+
+        $config = new ArrayObject([
+            'paginators' => [
+                'factories' => [
+                    'foo' => static fn (): AdapterInterface => $adapter,
+                ],
+            ],
+        ]);
+
+        $container     = new ServiceManager([
+            'services' => [
+                'config' => $config,
+            ],
+        ]);
+        $factory       = new AdapterPluginManagerFactory();
+        $pluginManager = $factory($container, 'whatever');
+
+        self::assertSame($adapter, $pluginManager->get('foo'));
     }
 }
